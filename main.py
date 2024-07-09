8eb6fb3c7e1994b4225703c04e8f6339699d93d9
Lan Wang <lan.lovesmaracuja@gmail.com>
2024-07-09 14:17:46 +0200

    updated ui

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+st.set_page_config(initial_sidebar_state="collapsed", layout="wide")
+
 IMG_FOLDER_PATH = "src/data/img/"
+
 
 def resize_image(url, size=(500, 550)):
     response = requests.get(url)
@@ -12,14 +15,64 @@ def resize_image(url, size=(500, 550)):
     img.thumbnail(size)
     return img
 
+
 def encode_image(image_path):
-        with open(image_path, "rb") as image_file:
-            return base64.b64encode(image_file.read()).decode()
-# homepage
-icon_image_path = IMG_FOLDER_PATH+"cine_logo.png"
-# st.image(icon_image_path)
-st.title("Welcome to CineTrip!")
-if st.button("Let's start"):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
+
+
+icon_image_path = IMG_FOLDER_PATH + "cine_logo.png"
+col11, col12, col13 = st.columns([4, 5, 1])
+
+with col11:
+    st.title("Welcome to CineTrip!")
+
+with col13:
+    st.image(icon_image_path, width=100)
+
+col21, col22, col23 = st.columns([1, 1, 1])
+with col21:
+    with st.container(height=320, border=True):
+        st.markdown("""
+        **What is CineTrip?**
+
+        Discover a whole new way to plan your travels with CineTrip, the innovative web-app that curates personalized tour trips based on your location and favorite movies. Whether you're a film buff dreaming of visiting iconic movie locations or an adventurer looking to explore destinations inspired by your beloved films, CineTrip has got you covered.
+        """)
+with col22:
+    with st.container(height=320, border=True):
+        st.markdown("""
+        **How to use?**
+        
+        Simply input your location and select movies you love, and let CineTrip craft a unique travel itinerary that brings your cinematic fantasies to life. From the bustling streets of New York featured in countless classics to the enchanting landscapes of Middle-earth, CineTrip turns your movie dreams into unforgettable travel experiences.
+        
+        **Step 1: Choose Your Location**
+        
+        You can either select any place in the world or use our auto-localization feature to find your current position. This flexibility ensures that CineTrip can cater to both your dream destinations and your immediate surroundings.
+
+        **Step 2: Rate Your Movies**
+        
+        Browse through a curated list of movies and rate them with "like," "didn't watch," or "unlike." Your preferences help us understand your tastes and interests.
+
+        **Step 3: Get Your Personalized Tour**
+        
+        Based on your movie choices and location, CineTrip will recommend an amazing movie location tour.
+        """)
+with col23:
+    with st.container(height=320, border=True):
+        st.markdown("""
+        **About Team 7 CineTrip**
+        
+        """)
+
+if st.button("✨Let's start!"):
     st.switch_page("pages/step1.py")
+
+import pandas as pd
+import numpy as np
+
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame(
+        np.random.randn(12, 5), columns=["a", "b", "c", "d", "e"]
+    )
 
 
--- pages/step3.py
+++ pages/step3.py
@@ -1,14 +1,16 @@
-import numpy as np
-import pandas as pd
-import pydeck as pdk
 import streamlit as st
-from opencage.geocoder import OpenCageGeocode
-import base64
+import pandas as pd
 import requests
 from PIL import Image
 from io import BytesIO
-from src.scripts.movieLensUtils import load_locations_csv, search_in_ml_latest_by_name, search_in_ml_hundred_by_id, load_links_data
-from src.scripts.recommender import MovieRecommender
+
+from src.scripts.movieLensUtils import search_in_ml_latest_by_name, load_links_data, search_in_ml_hundred_by_id, \
+    create_X, give_me_data, give_me_n_cold_start_movies
+
+movie_id_list = [x for x in give_me_n_cold_start_movies(15) if x not in [50, 181, 121]]
+
+if 'show' not in st.session_state:
+    st.session_state["show"] = -1
 
 
 def resize_image(url, size=(500, 550)):
@@ -16,6 +18,7 @@ def resize_image(url, size=(500, 550)):
     img = Image.open(BytesIO(response.content))
     img.thumbnail(size)
     return img
+
 
 def load_image(movieId):
     # print(movieId)
@@ -29,18 +32,29 @@ def load_image(movieId):
     else:
         return None
 
+
 def get_imdbID_by_movieId(movie_id):
+    movie_details = search_in_ml_hundred_by_id(movie_id)
+    # print(movie_details["title"].values[0])
+    other_movie_details = search_in_ml_latest_by_name(movie_details["title"].values[0])
+    # print(other_movie_details)
+    if len(other_movie_details["movieId"].values) == 0:
+        print("isssue with ", movie_details["title"].values[0])
+        return None
+    other_movie_id = other_movie_details["movieId"].values[0]
+    # print(other_movie_id)
     links_data = load_links_data()
-    # print("running ", movie_id, type(movie_id))
-    imdbId = links_data[links_data['movieId'] == str(movie_id)]['imdbId']
+    other_movie_id = str(other_movie_id)
+    imdbId = links_data[links_data['movieId'] == other_movie_id]['imdbId']
     # print(movie_id, "imdb le lo", imdbId.values[0])
     return imdbId.values[0]
+
 
 def get_movie_info_by_movieID(movie_id, api_key="9dc353f6"):
     movie_imdbID = get_imdbID_by_movieId(movie_id)
     # print(type(movie_imdbID))
     url = f"http://www.omdbapi.com/?i=tt{movie_imdbID}&plot=full&apikey={api_key}"
-    
+
     response = requests.get(url)
     # print(response)
     if response.status_code == 200:
@@ -48,187 +62,81 @@ def get_movie_info_by_movieID(movie_id, api_key="9dc353f6"):
     else:
         return {"error"}
 
-def get_lat_lon(address):
-    api_key = '8ff0d285a2074146b399833d726fc5af'
-    geocoder = OpenCageGeocode(api_key)
-    result = geocoder.geocode(address)
-    if result and len(result):
-        latitude = result[0]['geometry']['lat']
-        longitude = result[0]['geometry']['lng']
-        return latitude, longitude
-    else:
-        # garching:
-        return 48.2488721, 11.6532477
-
-locations_csv = load_locations_csv()
-example_dict = [{"address": "Baker Street Underground Station Undergound Ltd, Marylebone Rd, London NW1 5LJ",
-                 "tags": "Sherlock Holmes Street"},
-                {"address": "Praed St, London W2 1HU",
-                 "tags": "Paddington Bear"},
-                {"address": "85 Albert Embankment, London SE11 5AW",
-                 "tags": "Sky Fall"}
-                ]
-mr = None
-
-# TODO: change these if blocks for proper navigation
-if 'mr_object' in st.session_state: # change to 'not in' and create new object for mr/ but why tho (see later)
-    mr = st.session_state['mr_object']
-
-if 'city' not in st.session_state and 'country' not in st.session_state: # go back to step1
-    city = 'Los Angeles'
-    country = 'USA'
-else:
-    city, country = st.session_state['city'], st.session_state['country']
 
 
-if 'recs' not in st.session_state: # change to 'not in' and redirect to step 2
-    recs = [50, 174, 181, 79, 96, 56, 172, 204, 121, 195]
-    # Star Wars (1977), Raiders of the Lost Ark (1981), Return of the Jedi (1983),
-    #  Fugitive, The (1993), Terminator 2: Judgment Day (1991), Pulp Fiction (1994),
-    # Empire Strikes Back, The (1980), Back to the Future (1985), Independence Day (ID4) (1996), 
-    # Terminator, The (1984)
-else:   
-    recs = st.session_state['recs']
-    recs_names = st.session_state['recs_names']
-    
-recs_dict = {}
-filtered_recs = []
+colm1, colm2 = st.columns([1, 1])
+with colm1:
+    st.title("View your selections")
+with colm2:
+    if st.button("✨Show recommendations"):
+        st.switch_page("pages/step4.py")
 
-for i in recs:
-    movie_details = search_in_ml_hundred_by_id(i)
-    # print(movie_details)
-    if len(movie_details["title"].values) == 0:
-        continue
-    other_movie_details = search_in_ml_latest_by_name(movie_details["title"].values[0])
-    # print(other_movie_details)
-    if len(other_movie_details["movieId"].values) > 0:
-        filtered_recs.append(other_movie_details["movieId"].values[0])
-        recs_dict[other_movie_details["movieId"].values[0]] = movie_details["title"].values[0]
+if 'likes' in st.session_state and 'dislikes' in st.session_state and 'didntWatch' in st.session_state:
 
-# print("_",city,"_")
-# print(recs_dict)
-
-example_dict = mr.filter_dataframe(locations_csv, filtered_recs, city )
-    
-# print(len(example_dict))
-
-geocoded_data = []
-for item in example_dict:
-    lat, lon = get_lat_lon(item['address'])
-
-    geocoded_data.append({
-        "latitude": lat,
-        "longitude": lon,
-        "tags": item['tags'],
-        "movieId" : item['movieId']
-    })
-# print(geocoded_data)
-st.title('Results: recommended Locations')
-# Create Pydeck map
-if geocoded_data:
-    initial_view_state = pdk.ViewState(
-        latitude=geocoded_data[0]['latitude'],
-        longitude=geocoded_data[0]['longitude'],
-        zoom=11,
-        pitch=0,
-        height=350, width=600
-    )
-
-
-    def encode_image(image_path):
-        with open(image_path, "rb") as image_file:
-            return base64.b64encode(image_file.read()).decode()
-
-
-    icon_image_base64 = encode_image("src/data/img/location-pin.png")
-    icon_image_data = f"data:image/png;base64,{icon_image_base64}"
-    icon_data = {
-        "url": icon_image_data,
-        "width": 128,
-        "height": 128,
-        "anchorY": 128,
-    }
-    data = [
-        {
-            "coordinates": [data['longitude'], data['latitude']],
-            "tags": data['tags'],
-            "icon_data": icon_data,
-            "movieId": data['movieId']
+    st.markdown(
+        """
+        <style>
+        .stButton button {
+            width: 170px;
+            height: 90px;
+        .centered {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
         }
-        for data in geocoded_data
-    ]
-    icon_layer = pdk.Layer(
-        type="IconLayer",
-        data=data,
-        get_icon="icon_data",
-        get_size=4,
-        size_scale=15,
-        get_position="coordinates",
-        pickable=True,
+        </style>
+        """,
+        unsafe_allow_html=True
     )
 
-    tooltip = {"html": "{tags}"}
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1.5])
 
-    r = pdk.Deck(
-        map_style='mapbox://styles/mapbox/streets-v11',
-        initial_view_state=initial_view_state,
-        layers=[icon_layer],
-        tooltip=tooltip,
-    )
-    col1,col2=st.columns([2,1])
+    index = 0
     with col1:
-        # map
-        st.pydeck_chart(r, use_container_width=True)
+        st.markdown('<h2 class="centered">Likes</h2>', unsafe_allow_html=True)
+        for id in st.session_state['likes']:
+            movie_details = search_in_ml_hundred_by_id(id)
+            movie_name = movie_details["title"].values[0]
+            if st.button(movie_name, key=index):
+                st.session_state["show"] = id
+            index = index + 1
+
     with col2:
-        # address information
-        with st.container(height=350, border=True):
-            for item in example_dict:
-                with st.container(border=True):
-                    st.write(item["address"])
-                    st.image(resize_image(item['imgUrl'], (200, 120)))
-                    st.write("In "+recs_dict[item["movieId"]]+", ",item["tags"])
-                    
-                    # st.write(item["tags"])
+        st.markdown('<h2 class="centered">Dislikes</h2>', unsafe_allow_html=True)
+        for id in st.session_state['dislikes']:
+            movie_details = search_in_ml_hundred_by_id(id)
+            movie_name = movie_details["title"].values[0]
+            if st.button(movie_name, key=index):
+                st.session_state["show"] = id
+            index = index + 1
+    with col3:
+        st.markdown('<h2 class="centered">Didn\'t watch</h2>', unsafe_allow_html=True)
+        for id in st.session_state['didntWatch']:
+            movie_details = search_in_ml_hundred_by_id(id)
+            movie_name = movie_details["title"].values[0]
+            if st.button(movie_name, key=index):
+                st.session_state["show"] = id
+            index = index + 1
 
-                    
+    with col4:
+        with st.container(height=600, border=True):
+            if st.session_state["show"] != -1 and load_image(st.session_state["show"]):
+                st.image(load_image(st.session_state["show"]))
+                movie_json = get_movie_info_by_movieID(st.session_state["show"])
+                if movie_json:
+                    html_content = f"""
+                            <div style="max-width: 700px; max-height: 350px; overflow-x: hidden; overflow-y: auto; border: 1px solid #ccc; padding: 10px;">
+                            <h2>{movie_json['Title']}</h2>
+                            <p style="margin-bottom: 10px;"><strong>Rating: {movie_json['imdbRating']}   |   Year: {movie_json['Year']}   |   Region: {movie_json['Country']}</strong></p>
+        
+                            <p>{movie_json['Plot']}</p>
+        
+                            <ul>
+                                <li><strong>Genre:</strong> {movie_json['Genre']}</li>
+                                <li><strong>Director:</strong> {movie_json['Director']}</li>
+                                <li><strong>Actors:</strong> {movie_json['Actors']}</li>
+                            </ul>
+                            """
 
-    heading1, heading2 = st.columns([5, 1])
-
-
-    with heading1:
-        st.subheader("These are the movies that we think you like!", divider='rainbow')
-
-    len_rows = len(filtered_recs) // 4
-    actual_cut = len(filtered_recs) / 4
-    item_left = range(len_rows * 4,len(filtered_recs))
-    if len_rows == actual_cut:
-        for row in range(len_rows):
-            carousal_cols = st.columns([1,1,1,1])
-            for i in range(4):
-                with carousal_cols[i]:
-                    item = row* 4 + i
-                    with st.container(height=320,border=True):
-                        if load_image(filtered_recs[item]):
-                            st.image(load_image(filtered_recs[item]))
-                        st.write(recs_dict[filtered_recs[item]])
-    else:
-        for row in range(len_rows):
-            carousal_cols = st.columns([1,1,1,1])
-            for i in range(4):
-                with carousal_cols[i]:
-                    item = row* 4 + i
-                    with st.container(height=320,border=True):
-                        if load_image(filtered_recs[item]):
-                            st.image(load_image(filtered_recs[item]))
-                        st.write(recs_dict[filtered_recs[item]])
-        last_row = st.columns([1,1,1,1])      
-        for i in range(len(item_left)):
-            with last_row[i]:
-                item = item_left[i]
-                with st.container(height=320,border=True):
-                    if load_image(filtered_recs[item]):
-                        st.image(load_image(filtered_recs[item]))
-                    st.write(recs_dict[filtered_recs[item]])
-
-else:
-    st.write("No geocoded data available.")
+                st.markdown(html_content, unsafe_allow_html=True)
